1da002169dc4264342be6c3246105ac2a4eab74e
mafifi96 <prince.kassad1996@gmail.com>
2023-02-08 01:23:35 +0200

    add contracts page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr : false,
-    app: {
-        head: {
-          charset: 'utf-8',
-          viewport: 'width=device-width, initial-scale=1.0',
-          title: 'Payroll',
-          meta: [
-            { name: 'description', content: 'My amazing site.' }
-          ],
-        }
-      },
-      plugins :[
-        '~/plugin/persistedstate.js'
-      ],
-    modules : [
-        '@nuxtjs/tailwindcss',
-        '@pinia/nuxt'
-    ],
-    css : ['~/assets/css/font-awesome.min.css'],
-    
-})
+  ssr: false,
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1.0',
+      title: 'Payroll',
+      meta: [{
+        name: 'description',
+        content: 'My amazing site.'
+      }],
+    }
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt'
+  ],
+  css: [
+    '~/assets/css/font-awesome.min.css',
+  ],
 
+})
--- pages/contracts.vue
+++ pages/contracts.vue
@@ -5,18 +5,17 @@
             {{ title + ` | Employee` }}
         </Title>
     </Head>
-<Loader v-if="loading" />
-<CreateEmployee v-show="openModal" @close="openModal = false" />
+<Loader v-if="loading"></Loader>
+<CreateContract v-show="openModal" @close="openModal = false" />
 
     <div class="bg-white rounded p-3 text-center">
-        <h2 class="text-slate-600 capitalize font-semibold text-xl">employee</h2>
+        <h2 class="text-slate-600 capitalize font-semibold text-xl">contracts</h2>
     </div>
-    
 
     <div class="flex px-2 my-2 pb-5">
         <div class="w-full rounded-1 ">
             <div class="w-full flex justify-end items-center my-3 bg-[#fafafa] shadow-md rounded-sm p-3">
-                <button @click="openModal = true" class="apperance-none py-1 px-3 bg-indigo-600 text-white capitalize rounded  hover:ring-2 transition-all duration-300"><i class="fa fa-plus"></i> add employee</button>
+                <button @click="openModal = true" class="apperance-none py-1 px-3 bg-indigo-600 text-white capitalize rounded  hover:ring-2 transition-all duration-300"><i class="fa fa-plus"></i> create contract</button>
             </div>
 
             <div class="w-full bg-[#fafafa] shadow-md rounded-sm p-3">
@@ -24,29 +23,29 @@
                 <thead class="uppercase font-semibold text-sm text-gray-700 bg-zinc-100">
                     <tr>
                         <th style="width:5%">#</th>
-                        <th style="width:15%">name</th>
-                        <th style="width:15%">email</th>
-                        <th style="width:15%">position</th>
-                        <th style="width:25%">department</th>
-                        <th style="width:15%">joined</th>
+                        <th style="width:15%">title</th>
+                        <th style="width:15%" class="cursor-pointer" @click="showEmployee()">employee</th>
+                        <th style="width:15%">start</th>
+                        <th style="width:15%">end</th>
+                        <th style="width:15%">created</th>
                         <th style="width:5%">action</th>
                     </tr>
                 </thead>
                 <tbody class=" text-xs font-sans font-semibold text-gray-700">
-                    <tr v-for="e,index in employees" :key="index">
+                    <tr v-for="c,index in contracts" :key="index">
                         <td  class="font-semibold">{{ index+1 }}</td>
-                        <td >{{ e.name }}</td>
-                        <td >{{ e.email }}</td>
-                        <td >{{ e.position?.name || 'Waiting..' }}</td>
-                        <td >{{ e.department?.name }}</td>
-                        <td >{{ e.created_at }}</td>
+                        <td >{{ c.title }}</td>
+                        <td >{{ c.employee?.name }}</td>
+                        <td >{{ c.start_date || 'Waiting..' }}</td>
+                        <td >{{ c.end_date }}</td>
+                        <td >{{ c.created_at }}</td>
                         <td >
                             <div class="flex w-full h-fit justify-center items-center space-x-1">
                                 <button
                                 class="hover:text-indigo-600 px-2 py-1 text-sm"
-                                @click="edit(e.id)"><i class="fa fa-edit"></i></button> <button
+                                @click="edit(c.id)"><i class="fa fa-edit"></i></button> <button
                                 class=" hover:text-red-600 px-2 py-1 text-sm transition-colors duration-300"
-                                @click="destroy(e.id)"><i class="fa fa-trash-o"></i></button>
+                                @click="destroy(c.id)"><i class="fa fa-trash-o"></i></button>
                                 
                             </div>
                         </td>
@@ -65,33 +64,32 @@
 <script setup>
     
     import { onMounted } from 'vue';
-    import  CreateEmployee  from '~~/components/CreateEmployee.vue';
+    import  CreateContract  from '~~/components/CreateContract.vue';
     import Loader from '~~/components/Loader.vue'
     import { useStore } from '~~/store/Store'
 
     const token = useStore().token
     const openModal = ref(false)
-
     const { title } = useAppConfig()
-    const employees = ref([])
+    const contracts = ref([])
     const loading = ref(false)
+
     definePageMeta({
         layout: 'dashboard',
         middleware: 'auth'
     })
   
-    async function getEmployees()
+    async function getContracts()
     {
      
       loading.value = true
       
-      await  $fetch("http://127.0.0.1:8000/api/employees", {
+      await  $fetch("http://127.0.0.1:8000/api/contracts", {
         headers: {
             'Authorization': `Bearer ${token}`
         }
     }).then(res=>{
-        employees.value = res.data  
-        
+        contracts.value = res.data  
     }).catch(err=>{
         console.log("error........")
     }).finally(()=>{
@@ -103,7 +101,7 @@
 
     onMounted(() => {
 
-        getEmployees()
+        getContracts()
              
     })
 </script>
